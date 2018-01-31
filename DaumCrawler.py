37eb240e6950d8d3ab55cae27f16b7b005d3fe1d
= <=>
2018-01-31 16:37:24 +0900

    get rank news's url

---
+++ DaumCrawler.py
@@ -0,0 +1,28 @@
+import os
+import time
+from selenium import webdriver
+
+
+class DaumCrawler:
+    def __init__(self):
+        #os.environ['MOZ_HEADLESS'] = '1'
+        self.browser = webdriver.Firefox()
+        self.base_url = u'http://media.daum.net/ranking/bestreply/?regDate='
+
+    def get_targets(self, date):
+        query = str(date)
+        url = self.base_url + query
+        self.browser.get(url)
+        time.sleep(1)
+
+        li_list = self.browser.find_elements_by_xpath("//ul[contains(@class, 'list_news2')]//li")
+
+        urls = []
+        for li in li_list:
+            tag_a = li.find_element_by_tag_name('a')
+            urls.append(tag_a.get_attribute("href"))
+        return urls
+
+
+DC = DaumCrawler()
+urls = DC.get_targets(20180130)
